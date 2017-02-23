46e61317e09887bcf0f1347ba5b9e57e8f40a664
root <logicmoo@gmail.com>
2017-02-23 07:53:43 -0800

    v0.0.7

--- pack.pl
+++ pack.pl
@@ -1,11 +1,11 @@
 name(hilog).
 title('Allow Hilog Syntax in SWI-Prolog').
 
-version('0.0.1').
-download('https://github.com/TeamSPoon/pack_hilog/releases/*.zip').
+version('0.0.7').
+download('https://github.com/TeamSPoon/hilog/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 maintainer( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
-home('https://github.com/TeamSPoon/pack_hilog').
+home('https://github.com/TeamSPoon/hilog').
 autoload(true).
