c1818e6a9cfd9a91caece834fbdbe2974df22303
root <logicmoo@gmail.com>
2017-03-08 17:32:29 -0800

    v1.1.111

--- pack.pl
+++ pack.pl
@@ -1,11 +1,11 @@
 name(hilog).
-title('Allow Hilog Syntax in SWI-Prolog').
+title('XSB-Prolog''s Hilog Syntax in SWI-Prolog').
 
-version('0.0.8').
+version('1.1.111').
 download('https://github.com/TeamSPoon/hilog/releases/*.zip').
 
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
 home('https://github.com/TeamSPoon/hilog').
-autoload(true).
+autoload(false).
